35f690394156d78fddcf9aafa376d9e73169e15c
Brian Romanko <brian@bigbigbomb.com>
2012-12-18 11:50:23 -0600

    Enabling extended templates to override the functionality of the base template

--- test/index.js
+++ test/index.js
@@ -46,7 +46,64 @@ describe('Represent', function() {
       r.public.should.have.property('fields').with.lengthOf(1);
       var field = r.public.fields[0];
       field.should.have.property('name', 'firstName');
-    })
+    });
+
+    it('should create extended representations', function() {
+      var user = {
+        firstName: '',
+        lastName: ''
+      };
+
+      jiggler.define(user, 'public', [
+        jiggler.Field('firstName')
+      ]);
+      jiggler.define(user, 'extended', [
+          jiggler.Field('lastName')
+      ], {extends: 'public'});
+
+      var r = user[jiggler.JIGGLER_KEY];
+      r.extended.should.have.property('fields').with.lengthOf(2);
+      var field = r.extended.fields[0];
+      field.should.have.property('name', 'firstName');
+      field = r.extended.fields[1];
+      field.should.have.property('name', 'lastName');
+    });
+
+    it('should throw if the extended template does not exist', function() {
+      var user = {};
+
+      jiggler.define(user, 'public', []);
+
+      (function() {
+        jiggler.define(user, 'extended', [], {extends: 'undefined'});
+      }).should.throw();
+    });
+
+    it('should allow extended representations to override base implementations', function() {
+      var user = {
+        firstName: '',
+        lastName: ''
+      };
+
+      jiggler.define(user, 'public', [
+        jiggler.Field('firstName'),
+        jiggler.Field('lastName')
+      ]);
+      jiggler.define(user, 'extended', [
+        jiggler.Field('lastName', {
+          formatter: function(value) {
+            return value.charAt(0);
+          }
+        })
+      ], {extends: 'public'});
+
+      var r = user[jiggler.JIGGLER_KEY];
+      r.extended.should.have.property('fields').with.lengthOf(2);
+      var field = r.extended.fields[1];
+      field.should.have.property('name', 'lastName');
+      field.should.have.property('formatter');
+      field.formatter.should.be.a('function');
+    });
   });
 
   describe('jiggler', function() {
