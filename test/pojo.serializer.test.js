5b3e2d79330ed82f7b562862bf77939cce5bf35c
Brian Romanko <brian@bigbigbomb.com>
2012-12-18 10:34:09 -0600

    POJO serializing a simple property

--- test/pojo.serializer.test.js
+++ test/pojo.serializer.test.js
@@ -39,38 +39,22 @@ describe('POJO Serializer', function() {
       });
     });
 
-    it('should leverage a toObject method if the object has one', function(done) {
-      var user = new User();
-      var toObjectCalled = false;
-      user.toObject = function() {
-        toObjectCalled = true;
-        return {
-          key: 'value'
-        }
-      };
-      represent.define(user, 'public', []);
-
-      represent.as.pojo(user, 'public', {}, function(err, rep) {
-        should.not.exist(err);
-        should.exist(rep);
-        toObjectCalled.should.be.true;
-        done();
-      });
-    });
-
     it('should represent an instance', function(done) {
       var user = new User();
       user.firstName = 'Davos';
       user.lastName = 'Seaworth';
 
-      represent.define(user, 'public', []);
+      represent.define(user, 'public', [
+        represent.Field('firstName'),
+        represent.Field('lastName')
+      ]);
 
       represent.as.pojo(user, 'public', function(err, rep) {
         should.not.exist(err);
         should.exist(rep);
 
-        rep.should.not.have.property('firstName');
-        rep.should.not.have.property('lastName');
+        rep.should.have.property('firstName', 'Davos');
+        rep.should.have.property('lastName', 'Seaworth');
 
         done();
       });
