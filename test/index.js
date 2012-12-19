58ab40f0d5b36c69331c4f4418653dd72fa8ccf2
Brian Romanko <brian@bigbigbomb.com>
2012-12-19 12:09:34 -0600

    Properly representing all objects except for embedded jiggler objects

--- test/index.js
+++ test/index.js
@@ -110,13 +110,6 @@ describe('Jiggler', function() {
   });
 
   describe('as', function() {
-
-    var Car = function() {
-      this.year = 2012;
-      this.make = '';
-    };
-
-
     it('should require a valid template name', function() {
       var User = function() {
         this.firstName = '';
@@ -144,198 +137,227 @@ describe('Jiggler', function() {
         done();
       });
     });
-//
-//    it('should represent an instance with simple properties', function(done) {
-//      var User = function() {
-//        this.firstName = '';
-//        this.lastName = '';
-//      };
-//      var user = new User();
-//      user.firstName = 'Davos';
-//      user.lastName = 'Seaworth';
-//
-//      J.define(User, 'public', [
-//        J.Field('firstName'),
-//        J.Field('lastName')
-//      ]);
-//
-//      J.represent(user, 'public', function(err, rep) {
-//        should.not.exist(err);
-//        should.exist(rep);
-//
-//        rep.should.have.property('firstName', 'Davos');
-//        rep.should.have.property('lastName', 'Seaworth');
-//
-//        done();
-//      });
-//    });
-//
-//    it('should represent an instance with object properties', function(done) {
-//      var user = new User();
-//      user.firstName = 'Davos';
-//      user.car = {
-//        year: 2001,
-//        make: 'Ford'
-//      };
-//
-//      J.define(user, 'public', [
-//        J.Field('firstName'),
-//        J.Field('car')
-//      ]);
-//
-//      J.as.pojo(user, 'public', function(err, rep) {
-//        should.not.exist(err);
-//        should.exist(rep);
-//
-//        rep.should.have.property('firstName', 'Davos');
-//        rep.should.have.property('car');
-//
-//        done();
-//      });
-//    });
-//
-//    it('should represent an instance property with an alternative template', function(done) {
-//      var user = new User();
-//      user.firstName = 'Davos';
-//      var car = new Car();
-//      car.make = 'BMW';
-//      user.car = car;
-//
-//      J.define(user, 'public', [
-//        J.Field('firstName'),
-//        J.Field('car', {template: 'different'})
-//      ]);
-//      J.define(car, 'different', []);
-//
-//      J.as.pojo(user, 'public', function(err, rep) {
-//        should.not.exist(err);
-//        should.exist(rep);
-//
-//        rep.should.have.property('firstName', 'Davos');
-//        rep.should.have.property('car');
-//        rep.car.should.not.have.property('year');
-//        rep.car.should.not.have.property('make');
-//
-//        done();
-//      });
-//    });
-//
-//    it('should represent an array', function(done) {
-//      var user = new User();
-//      user.firstName = 'Davos';
-//      user.lastName = 'Seaworth';
-//      var user2 = new User();
-//      user2.firstName = 'Sandor';
-//      user2.lastName = 'Clegane';
-//
-//      var fields = [
-//        J.Field('firstName'),
-//        J.Field('lastName')
-//      ];
-//      J.define(user, 'public', fields);
-//      J.define(user2, 'public', fields);
-//
-//      J.as.pojo([user, user2], 'public', function(err, rep) {
-//        should.not.exist(err);
-//        should.exist(rep);
-//
-//        rep.should.be.an.instanceOf(Array);
-//        rep.should.have.lengthOf(2);
-//        rep[0].should.have.property('firstName', 'Davos');
-//        rep[0].should.have.property('lastName', 'Seaworth');
-//        rep[1].should.have.property('firstName', 'Sandor');
-//        rep[1].should.have.property('lastName', 'Clegane');
-//
-//        done();
-//      });
-//    });
-//
-//    it('should represent an instance with an array property', function(done) {
-//      var user = {
-//        test: ['one', 'two']
-//      };
-//
-//      J.define(user, 'public', [
-//        J.Field('test')
-//      ]);
-//
-//      J.as.pojo(user, 'public', function(err, rep) {
-//        should.not.exist(err);
-//        should.exist(rep);
-//
-//        rep.should.have.property('test').with.lengthOf(2);
-//        rep.test.should.include('one');
-//        rep.test.should.include('two');
-//
-//        done();
-//      });
-//    });
-//
-//    it('should represent an instance with a formatter', function(done) {
-//      var user = new User();
-//      user.firstName = 'Davos';
-//      user.lastName = 'Seaworth';
-//
-//      J.define(user, 'public', [
-//        J.Field('firstName', {
-//          formatter: function(value) {
-//            return value.charAt(0);
-//          }
-//        })
-//      ]);
-//
-//      J.as.pojo(user, 'public', function(err, rep) {
-//        should.not.exist(err);
-//        should.exist(rep);
-//
-//        rep.should.have.property('firstName', 'D');
-//
-//        done();
-//      });
-//    });
-//
-//    it('should strip undefined values by default', function(done) {
-//      var user = new User();
-//      user.firstName = undefined;
-//      user.lastName = 'Seaworth';
-//
-//      J.define(user, 'public', [
-//        J.Field('firstName'),
-//        J.Field('lastName')
-//      ]);
-//
-//      J.as.pojo(user, 'public', function(err, rep) {
-//        should.not.exist(err);
-//        should.exist(rep);
-//
-//        ('firstName' in rep).should.equal(false);
-//        rep.should.have.property('lastName');
-//
-//        done();
-//      });
-//    });
-//
-//    it('should not strip undefined values if stripUndefined is set to false', function(done) {
-//      var user = new User();
-//      user.firstName = undefined;
-//      user.lastName = 'Seaworth';
-//
-//      J.define(user, 'public', [
-//        J.Field('firstName'),
-//        J.Field('lastName')
-//      ]);
-//
-//      J.as.pojo(user, 'public', {stripUndefined: false}, function(err, rep) {
-//        should.not.exist(err);
-//        should.exist(rep);
-//
-//        ('firstName' in rep).should.equal(true);
-//        should.equal(undefined, rep.firstName);
-//        rep.should.have.property('lastName');
-//
-//        done();
-//      });
-//    });
+
+    it('should represent an instance with simple properties', function(done) {
+      var User = function() {
+        this.firstName = '';
+        this.lastName = '';
+      };
+      var user = new User();
+      user.firstName = 'Davos';
+      user.lastName = 'Seaworth';
+
+      J.define(User, 'public', [
+        J.Field('firstName'),
+        J.Field('lastName')
+      ]);
+
+      User[J.JIGGLER_KEY].as.public(user, function(err, rep) {
+        should.not.exist(err);
+        should.exist(rep);
+
+        rep.should.have.property('firstName', 'Davos');
+        rep.should.have.property('lastName', 'Seaworth');
+
+        done();
+      });
+    });
+
+    it('should represent an instance with object properties', function(done) {
+      var User = function() {
+        this.firstName = '';
+        this.lastName = '';
+      };
+      var user = new User();
+      user.firstName = 'Davos';
+      user.car = {
+        year: 2001,
+        make: 'Ford'
+      };
+
+      J.define(User, 'public', [
+        J.Field('firstName'),
+        J.Field('car')
+      ]);
+
+      User[J.JIGGLER_KEY].as.public(user, function(err, rep) {
+        should.not.exist(err);
+        should.exist(rep);
+
+        rep.should.have.property('firstName', 'Davos');
+        rep.should.have.property('car');
+
+        done();
+      });
+    });
+
+    it('should represent an instance property with an alternative template', function(done) {
+      var User = function() {
+        this.firstName = '';
+        this.lastName = '';
+      };
+      var Car = function() {
+        this.year = 2012;
+        this.make = '';
+      };
+      var user = new User();
+      user.firstName = 'Davos';
+      var car = new Car();
+      car.make = 'BMW';
+      user.car = car;
+
+      J.define(User, 'public', [
+        J.Field('firstName'),
+        J.Field('car', {template: 'different'})
+      ]);
+      J.define(Car, 'different', []);
+
+      User[J.JIGGLER_KEY].as.public(user, function(err, rep) {
+        should.not.exist(err);
+        should.exist(rep);
+
+        rep.should.have.property('firstName', 'Davos');
+        rep.should.have.property('car');
+        rep.car.should.not.have.property('year');
+        rep.car.should.not.have.property('make');
+
+        done();
+      });
+    });
+
+    it('should represent an array', function(done) {
+      var User = function() {
+        this.firstName = '';
+        this.lastName = '';
+      };
+      var user = new User();
+      user.firstName = 'Davos';
+      user.lastName = 'Seaworth';
+      var user2 = new User();
+      user2.firstName = 'Sandor';
+      user2.lastName = 'Clegane';
+
+      var fields = [
+        J.Field('firstName'),
+        J.Field('lastName')
+      ];
+      J.define(User, 'public', fields);
+
+      User[J.JIGGLER_KEY].as.public([user, user2], function(err, rep) {
+        should.not.exist(err);
+        should.exist(rep);
+
+        rep.should.be.an.instanceOf(Array);
+        rep.should.have.lengthOf(2);
+        rep[0].should.have.property('firstName', 'Davos');
+        rep[0].should.have.property('lastName', 'Seaworth');
+        rep[1].should.have.property('firstName', 'Sandor');
+        rep[1].should.have.property('lastName', 'Clegane');
+
+        done();
+      });
+    });
+
+    it('should represent an instance with an array property', function(done) {
+      var User = function() {
+      };
+      var user = {
+        test: ['one', 'two']
+      };
+
+      J.define(User, 'public', [
+        J.Field('test')
+      ]);
+
+      User[J.JIGGLER_KEY].as.public(user, function(err, rep) {
+        should.not.exist(err);
+        should.exist(rep);
+
+        rep.should.have.property('test').with.lengthOf(2);
+        rep.test.should.include('one');
+        rep.test.should.include('two');
+
+        done();
+      });
+    });
+
+    it('should represent an instance with a formatter', function(done) {
+      var User = function() {
+        this.firstName = '';
+        this.lastName = '';
+      };
+      var user = new User();
+      user.firstName = 'Davos';
+      user.lastName = 'Seaworth';
+
+      J.define(User, 'public', [
+        J.Field('firstName', {
+          formatter: function(value) {
+            return value.charAt(0);
+          }
+        })
+      ]);
+
+      User[J.JIGGLER_KEY].as.public(user, function(err, rep) {
+        should.not.exist(err);
+        should.exist(rep);
+
+        rep.should.have.property('firstName', 'D');
+
+        done();
+      });
+    });
+
+    it('should strip undefined values by default', function(done) {
+      var User = function() {
+        this.firstName = '';
+        this.lastName = '';
+      };
+      var user = new User();
+      user.firstName = undefined;
+      user.lastName = 'Seaworth';
+
+      J.define(User, 'public', [
+        J.Field('firstName'),
+        J.Field('lastName')
+      ]);
+
+      User[J.JIGGLER_KEY].as.public(user, function(err, rep) {
+        should.not.exist(err);
+        should.exist(rep);
+
+        ('firstName' in rep).should.equal(false);
+        rep.should.have.property('lastName');
+
+        done();
+      });
+    });
+
+    it('should not strip undefined values if stripUndefined is set to false', function(done) {
+      var User = function() {
+        this.firstName = '';
+        this.lastName = '';
+      };
+      var user = new User();
+      user.firstName = undefined;
+      user.lastName = 'Seaworth';
+
+      J.define(User, 'public', [
+        J.Field('firstName'),
+        J.Field('lastName')
+      ]);
+
+      User[J.JIGGLER_KEY].as.public(user, {stripUndefined: false}, function(err, rep) {
+        should.not.exist(err);
+        should.exist(rep);
+
+        ('firstName' in rep).should.equal(true);
+        should.equal(undefined, rep.firstName);
+        rep.should.have.property('lastName');
+
+        done();
+      });
+    });
 //  });
 //
 //  describe('as', function() {
